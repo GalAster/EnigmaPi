cfe75e2843930e463cafc3d83c03a17053a58cc3
GalAster <galaster@foxmail.com>
2018-05-17 15:15:03 +0800

    重构::Code Formatting

--- algorithm/EnigmaPi.m
+++ algorithm/EnigmaPi.m
@@ -16,8 +16,8 @@ $SavePath = FileNameJoin[{$UserBaseDirectory, "ApplicationData", "EnigmaPi"}];
 
 
 If[!FileExistsQ@#, CreateDirectory[#]]& /@ {
-  $SavePath,
-  FileNameJoin@{$SavePath, "Pi"}
+	$SavePath,
+	FileNameJoin@{$SavePath, "Pi"}
 };
 
 
@@ -30,20 +30,20 @@ If[!FileExistsQ@#, CreateDirectory[#]]& /@ {
 
 SetAttributes[EvaluateConstant, HoldAll];
 EvaluateConstant[offset_, C_ : Pi] := Block[
-  {eval, dir, name, path},
-  dir = FileNameJoin[{$SavePath, ToString[FromDigits[RealDigits[N@C][[1, 1 ;; 10]]]]}];
-  name = StringJoin[ToString[offset + 1], "-", ToString[offset + $BlockSize], ".calc.m"];
-  path = FileNameJoin[{dir, name}];
-  If[!FileExistsQ@dir, CreateDirectory[dir]];
-  If[FileExistsQ@path, Echo[name, "Skip: " ];Return[path]];
-  eval = RealDigits[Pi, 10, $BlockSize + $Redundancy, -1 - offset];
-  Export[path, <|
-    "Constant" -> Hold[C],
-    "Type" -> "Original Calculation Results",
-    "Offset" -> Last[eval],
-    "Time" -> Now,
-    "Data" -> BinarySerialize[First@eval, PerformanceGoal -> "Size"]
-  |>]
+	{eval, dir, name, path},
+	dir = FileNameJoin[{$SavePath, ToString[FromDigits[RealDigits[N@C][[1, 1 ;; 10]]]]}];
+	name = StringJoin[ToString[offset + 1], "-", ToString[offset + $BlockSize], ".calc.m"];
+	path = FileNameJoin[{dir, name}];
+	If[!FileExistsQ@dir, CreateDirectory[dir]];
+	If[FileExistsQ@path, Echo[name, "Skip: " ];Return[path]];
+	eval = RealDigits[Pi, 10, $BlockSize + $Redundancy, -1 - offset];
+	Export[path, <|
+		"Constant" -> Hold[C],
+		"Type" -> "Original Calculation Results",
+		"Offset" -> Last[eval],
+		"Time" -> Now,
+		"Data" -> BinarySerialize[First@eval, PerformanceGoal -> "Size"]
+	|>]
 ];
 
 
@@ -53,26 +53,26 @@ EvaluateConstant[offset_, C_ : Pi] := Block[
 
 CountFirst = Append[List @@ First[#], Length[#]]&;
 CountAll[data_, length_ : 1] := Block[
-  {pat = FromDigits /@ Take[Partition[data, length, 1], $BlockSize]},
-  CountFirst /@ GatherBy[Thread[pat -> Range[$BlockSize]], First]
+	{pat = FromDigits /@ Take[Partition[data, length, 1], $BlockSize]},
+	CountFirst /@ GatherBy[Thread[pat -> Range[$BlockSize]], First]
 ];
 
 
 SetAttributes[CountExportTxt, HoldAll];
 CountExportTxt[file_, offset_ : 0, C_ : Pi] := Block[
-  {name, path, input, data, ans},
-  name = StringJoin[ToString /@ {offset + 1, "-", offset + $BlockSize, ".data.mx"}];
-  path = FileNameJoin[{$SavePath, ToString@C, name}];
-  If[FileExistsQ@path, Echo[name, "Skip: " ];Return[path]];
-  input = OpenRead[file];
-  SetStreamPosition[input, 2 + offset];
-  data = ToExpression /@ Read[input, ConstantArray[Character, $BlockSize + $Redundancy]];
-  ans = Flatten[CountAll[data, #]& /@ Range[12], 1];
-  Export[path, <|
-    "Constant" -> Hold[C],
-    "Type" -> "Count Data",
-    "Offset" -> offset + 1,
-    "Time" -> Now,
-    "Data" -> BinarySerialize[ans, PerformanceGoal -> "Size"]
-  |>]
+	{name, path, input, data, ans},
+	name = StringJoin[ToString /@ {offset + 1, "-", offset + $BlockSize, ".data.mx"}];
+	path = FileNameJoin[{$SavePath, ToString@C, name}];
+	If[FileExistsQ@path, Echo[name, "Skip: " ];Return[path]];
+	input = OpenRead[file];
+	SetStreamPosition[input, 2 + offset];
+	data = ToExpression /@ Read[input, ConstantArray[Character, $BlockSize + $Redundancy]];
+	ans = Flatten[CountAll[data, #]& /@ Range[12], 1];
+	Export[path, <|
+		"Constant" -> Hold[C],
+		"Type" -> "Count Data",
+		"Offset" -> offset + 1,
+		"Time" -> Now,
+		"Data" -> BinarySerialize[ans, PerformanceGoal -> "Size"]
+	|>]
 ]
